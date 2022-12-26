b0cf3dcf513bcc49f8d6e83b72aa700994ee35f9
Tsai Amy <amy19651965@gmail.com>
2022-12-26 16:11:39 +0800

    new

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ if(process.env.NODE_ENV === 'development') {
     app.use(cors());
 }
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 app.use(bodyParser.json());
