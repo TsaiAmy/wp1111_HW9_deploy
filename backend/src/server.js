9c08ea6e464dbb0ccb2816f99c0b57834eb93efc
Tsai Amy <amy19651965@gmail.com>
2022-12-26 16:07:13 +0800

    edit

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import bodyParser from 'body-parser'
 db.connect();
 
 const app = express();
+
 const port = process.env.PORT || 4000;
 
 // app.get('/', (req, res) => {
@@ -15,7 +16,20 @@ const port = process.env.PORT || 4000;
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`)
 );
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(cors());
+}
+
 app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 app.use(bodyParser.json());
+
+if(process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    })
+}
